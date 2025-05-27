18683060ea532ba2d160314fc3e3b8ff9c630ef9
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2025-05-27 22:23:57 +0100

    Basic Program

--- dist/index.js
+++ dist/index.js
@@ -2,13 +2,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const commander_1 = require("commander");
+let print = console.log;
 const program = new commander_1.Command();
 program
     .name("hello-world")
     .description("A simple Hello World CLI tool")
-    .version("0.1.0")
-    .action(() => {
-    console.log("Hello World!");
-    console.log("UwU");
+    .version("0.1.0");
+program.command("split")
+    .description('Split a string into substrings and display as an array')
+    .argument('<string>', 'string to split')
+    .argument('<string>', 'another str')
+    .option('--first', 'display just the first substring')
+    .option('-s, --separator <char>', 'separator character', ',')
+    .action((str, str2, options) => {
+    print(str, str2);
+    print(options);
+    // process.cwd()
 });
 program.parse();
--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,27 @@
 
 import { Command } from "commander";
 
+let print = console.log
+
 const program = new Command();
 
 program
   .name("hello-world")
   .description("A simple Hello World CLI tool")
   .version("0.1.0")
-  .action(() => {
-    console.log("Hello World!");
-    console.log("UwU")
+
+program.command("split")
+  .description('Split a string into substrings and display as an array')
+  .argument('<string>', 'string to split')
+  .argument('<string>', 'another str')
+  .option('--first', 'display just the first substring')
+  .option('-s, --separator <char>', 'separator character', ',')
+  .action((str, str2, options: {[key: string]: any}) => {
+    print(str, str2)
+    print(options)
+
+    // Get Current directory process.cwd()
   });
+
 
 program.parse();
