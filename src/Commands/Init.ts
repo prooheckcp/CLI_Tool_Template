bc39ecdc51c9cf1d412c4e8dddcfaffd9a370405
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2025-06-01 19:14:20 +0100

    Finishing structure for cloning

--- src/Commands/Init.ts
+++ src/Commands/Init.ts
@@ -1,17 +1,25 @@
+import path from "path"
+import { rename, readdir, rm } from 'fs/promises'
+import { existsSync } from "fs"
+
 const { exec } = require('child_process')
 
-const TARGET_REPO: string = "prooheckcp/RobloxTemplate"
+const TARGET_REPO: string = "https://github.com/prooheckcp/RobloxTemplate"
+const TEMP_NAME: string = "repo"
 
 const init = (program: any)=> {
-  
     program.command("init")
     .description('Initiates a git project and extracts it into the current folder')
-    .option('--first', 'display just the first substring')
+    .argument("[path]", 'string with the path to the directory')
     .option('-p, --path <string>', 'custom path')
-    .action((options: {[key: string]: any}) => {
-        console.log("Options: ", options)
-        new Promise<void>(resolve =>{
-            exec(`git clone ${TARGET_REPO} ${process.cwd()}`, (error: any, stdout: string, stderr: string) => {
+    .action(async (pathArg: string, options: {[key: string]: any}) => {
+
+        /* 
+        let targetPath: string = pathArg || options.path || process.cwd()
+        let pastePath: string = path.join(targetPath, TEMP_NAME)
+
+        await new Promise<void>(resolve =>{
+            exec(`git clone ${TARGET_REPO} ${pastePath}`, (error: any, _stdout: string, stderr: string) => {
                 if (error) {
                     console.error(`exec error: ${error.message}`)
                     return
@@ -19,10 +27,29 @@ const init = (program: any)=> {
                 if (stderr)
                     console.error(`stderr: ${stderr}`);
 
-                console.log(`stdout: ${stdout}`);
                 resolve()
             })
-         })
+         }).then<void>(async _ => {
+            const files: string[] = await readdir(pastePath)
+
+            files.forEach(async (file: string) => {
+                // Need to check if it exists and if yes then delete it?
+                let moveToPath: string = path.join(pastePath, "..", file)
+                let baseName =  path.basename(moveToPath)
+                
+                if (baseName == ".git")
+                    return
+
+                if (existsSync(moveToPath))
+                    await rm(moveToPath, { recursive: true, force: true })
+
+                await rename(path.join(pastePath, file), moveToPath)
+            })
+         }).then(async _ => {
+            await rm(pastePath, { recursive: true, force: true })
+         })        
+        */
+
     });
 
 }
