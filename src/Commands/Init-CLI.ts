b803a78b909f4af782ab91942907ca33a5f443d3
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2025-06-02 22:32:08 +0100

    Delete test content

--- src/Commands/Init-CLI.ts
+++ src/Commands/Init-CLI.ts
@@ -1,10 +1,10 @@
 import {extractRepo} from "./../Functions/extractRepo"
 
-const TARGET_REPO: string = "https://github.com/prooheckcp/RobloxTemplate"
+const TARGET_REPO: string = "https://github.com/prooheckcp/CLI_Tool_Template"
 
 const init = (program: any)=> {
-    program.command("init")
-    .description('Initiates a git project and extracts it into the current folder')
+    program.command("init-cli")
+    .description('Initiates a CLI project')
     .argument("[path]", 'string with the path to the directory')
     .option('-p, --path <string>', 'custom path')
     .action(async (pathArg: string, options: {[key: string]: any}) => {
